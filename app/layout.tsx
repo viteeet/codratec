d8c8360d963974cae33da6339e8f3d3c79d1f0a0
Victor Hugo <191982238+viteeet@users.noreply.github.com>
2026-08-27 10:06:17 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,7 +60,7 @@ export const metadata: Metadata = {
 export const viewport = {
   width: 'device-width',
   initialScale: 1,
-  themeColor: '#0f172a',
+  themeColor: '#ffffff',
 };
 
 export default function RootLayout({
